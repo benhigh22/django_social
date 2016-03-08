d9109f4eb550ac8f90e48b4713cd685fa2ecb7be
benhigh22 <benjaming.high@gmail.com>
2016-03-08 15:17:56 -0500

    About to add friend ability

--- social_app/models.py
+++ social_app/models.py
@@ -9,12 +9,13 @@ class UserProfile(models.Model):
 
 class Team(models.Model):
     team_name = models.CharField(max_length=50)
+    time_made = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.team_name
 
     class Meta:
-        ordering = ['-team_name']
+        ordering = ['-time_made']
 
 class Topic(models.Model):
     title = models.CharField(max_length=50)
